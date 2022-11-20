788590acc1994391254a15a2a5249ffca67a6642
Imad Mimouni <imimouni@e2r5p13.1337.ma>
2022-11-20 11:04:39 +0100

    m

--- ft_printf.c
+++ ft_printf.c
@@ -22,3 +22,29 @@ int	main()
 
 	return 0;
 }
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdarg.h>
+//#include <unistd.h>
+//#include <string.h>
+//
+//
+//void    ft_printf(int n, ...)
+//{
+//    va_list    ap;
+//
+//    va_start(ap, n);
+//    printf("%s",va_arg(ap, char *));
+//    printf("%s",va_arg(ap, char *));
+//    printf("%s",va_arg(ap, char *));
+//    printf("%s",va_arg(ap, char *));
+//    va_end(ap);
+//}
+//
+//int    main()
+//{
+//    ft_printf(0,"imad"," mimouni", " mimouni");
+//
+//    return 0;
+//}
